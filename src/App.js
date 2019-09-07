67d8bf8c4edd23775ad593ebdee4ea4328237dd2
hayderjebur <hayderjabur1983@gmail.com>
2019-09-07 08:53:36 -0700

    update menu-item withRouter HOC and

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Swich, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+
+const HatsPage = () => {
+  return <div>HatsPage</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <swich>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/hats" exact component={HatsPage} />
+      </swich>
     </div>
   );
 }
