b765875dc020ed45f846a10944e4c20af4325fc0
hayderjebur <hayderjabur1983@gmail.com>
2019-09-07 12:46:00 -0700

    add heaer component and links to home page, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Swich, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 import ShopPage from "./pages/shop/Shop.component";
+import Header from "./component/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <swich>
         <Route path="/" exact component={HomePage} />
         <Route path="/shop" exact component={ShopPage} />
